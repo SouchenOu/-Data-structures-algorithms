01267822426b8d83e1f5a5841ca097e44894b11d
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-01-06 13:17:35 +0100

    start solving some problems

---
+++ problems_solve/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/****Objectives
+ * 
+To review the key ideas of computer science, programming, and problem-solving using the C++ language.
+
+To understand abstraction and the role it plays in the problem-solving process.
+
+To develop understanding of Big O notation and other important techniques of algorithm analysis.
+
+To develop comfort with standard data structures including queues, stacks, and trees.
+
+To become conversant with sorting algorithms, including the advantages and disadvantages of each.
+
+To learn to write and analyze recursive programs, functions, and methods.
+
+To learn to traverse and to implement linked data structures such as linked lists and trees.
+
+*/
+
+int main()
+{
+
+}
