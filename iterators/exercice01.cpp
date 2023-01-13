3db3c0ab4051c25f681e32c0883a09ba41bed4c5
Soukaina Ouchen <souchen@e1r7p1.1337.ma>
2023-01-13 15:06:30 +0100

    another exercice iterators

---
+++ iterators/exercice01.cpp
@@ -0,0 +1,43 @@
+// C++ program to demonstrate iterators
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    // Declaring a vector
+    vector<int> v = { 1, 2, 3 };
+ 
+    // Declaring an iterator
+    vector<int>::iterator i;
+ 
+    int j;
+ 
+    // Inserting element using iterators
+    for (i = v.begin(); i != v.end(); ++i) {
+        if (i == v.begin()) {
+            i = v.insert(i, 5);
+            // inserting 5 at the beginning of v
+        }
+    }
+     
+    // v contains 5 1 2 3
+ 
+    // Deleting a element using iterators
+    for (i = v.begin(); i != v.end(); ++i) {
+        if (i == v.begin() + 1) {
+            i = v.erase(i);
+            // i now points to the element after the
+            // deleted element
+        }
+    }
+     
+    // v contains 5 2 3
+ 
+    // Accessing the elements using iterators
+    for (i = v.begin(); i != v.end(); ++i) {
+        cout << *i << " ";
+    }
+ 
+    return 0;
+}
